ac70b71020930603889cfd218cc5541a4dc888a7
Erik Nisbet <eriknis@student.chalmers.se>
2025-03-20 21:08:39 +0100

    Add variable for socket file descriptor and print it, add error handling. Next step, understand IPv4 and UDP

--- udp/udp_server.go
+++ udp/udp_server.go
@@ -8,7 +8,12 @@ import (
 
 
 func main() {
-	
-	fmt.Println(syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP))
-	
+	//  Create socket
+	// IPv4, UDP Socket, UDP
+	udpSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		fmt.Println("Error creating socket", err)
+		return
+	}
+	fmt.Println("Udp socket: ", udpSocket)
 }
