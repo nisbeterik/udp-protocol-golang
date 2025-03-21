3de9a89ba08042bcd0d6197f7d7ac15a60cae934
Erik Nisbet <eriknis@student.chalmers.se>
2025-03-21 13:31:08 +0100

    Add udp packet struct and save Recvfrom return values into packet object

--- udp/udp_server.go
+++ udp/udp_server.go
@@ -6,8 +6,16 @@ import (
 	"syscall"
 	)
 
+type udpPacket struct {
+	BytesRecieved int
+	ClientAddress syscall.Sockaddr
+	Err error
+}
+
 
 func main() {
+
+	buffer := make([]byte, 512) // buffer to store packet data in
 	//  Create socket
 	// IPv4, UDP Socket, UDP
 	udpSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
@@ -15,12 +23,18 @@ func main() {
 		fmt.Println("Error creating socket", err)
 		return
 	}
-
+	// set address that socket will bind to
 	addr := &syscall.SockaddrInet4{
 		Port: 8080,					// bind socket to port 8080
 		Addr: [4]byte{127,0,0,1},	// set it to interface only with localhost
 	}
+
+	
 	
 	syscall.Bind(udpSocket, addr) // bind the socket to the address/port. this will route traffic to the socket
 
+	bytes, client_address, err :=	syscall.Recvfrom(udpSocket, buffer, 0)
+
+	tempPacket := udpPacket{bytes, client_address, err}
+	fmt.Println(tempPacket)
 }
