85854de6bb165eb8cabf413b4b71216cecaaf0dc
Erik Nisbet <eriknis@student.chalmers.se>
2025-03-21 19:16:15 +0100

    Add bindSocket function to bind socket

--- udp/server/udp_server.go
+++ udp/server/udp_server.go
@@ -44,16 +44,18 @@ func main() {
 
 
 
-	/*
-	fmt.Println("Binding socket..:")	
-	err = syscall.Bind(udpSocket, addr) // bind the socket to the address/port. this will route traffic to the socket
+	counter := 0
+	fmt.Println(counter)
+
+	err = bindSocket(&udpSocket)
 	if err != nil {
-		fmt.Println("Error binding sockets:", err)
+		fmt.Println("Error binding socket:", err)
 		return
 	}
 
+
 	fmt.Println("Listening for packets...")
-	bytes, clientAddress, err :=	syscall.Recvfrom(udpSocket, buffer, 0)
+	bytes, clientAddress, err :=	syscall.Recvfrom(udpSocket.FileDescriptor, buffer, 0)
 	if err != nil {
 		fmt.Println("Error receiving data:", err)
 		return
@@ -71,8 +73,8 @@ func main() {
 	fmt.Println(tempPacket)
 	message := string(buffer[:tempPacket.BytesRecieved])
 	fmt.Println("Message from client: " + message)
-	syscall.Close(udpSocket)
-	fmt.Println("Closing server...")*/
+	syscall.Close(udpSocket.FileDescriptor) // close socket when server is done
+	fmt.Println("Closing server...")
 }
 
 
@@ -96,7 +98,7 @@ func setAddress(udpSocket *UDPSocket, port int, ip [4]byte) error {
 
 	fmt.Println("Setting address...")	
 	if port < 0 || port > 0xFFFF {
-		return fmt.Errorf("Invalid port number: %d, must between 0 and %d", port, 0xFFFF)
+		return fmt.Errorf("invalid port number: %d, must between 0 and %d", port, 0xFFFF)
 	}
 
 	udpSocket.ServerAddress.Port = port
@@ -107,8 +109,7 @@ func setAddress(udpSocket *UDPSocket, port int, ip [4]byte) error {
 
 func bindSocket(udpSocket *UDPSocket) error {
 	fmt.Println("Binding socket...")
-	var err error
-	err = syscall.Bind(udpSocket.FileDescriptor, &udpSocket.ServerAddress)
+	err := syscall.Bind(udpSocket.FileDescriptor, &udpSocket.ServerAddress)
 	if err != nil {
 		fmt.Println("Error binding socket:", err)
 		return err
